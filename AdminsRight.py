062887e589cb6874bf285b845c38cd6e120ddf4d
n♡user <nloveuser@gmail.com>
2025-09-03 20:37:51 +0500

    Update AdminsRight.py

--- AdminsRight.py
+++ AdminsRight.py
@@ -160,7 +160,7 @@ class AdminConfigurator(loader.Module):
         lines = []
         for key, desc in (await self._all_rights()).items():
             state = "<emoji document_id=5258453452631056344>✅️</emoji>" if conf.get(key, False) else "<emoji document_id=5260342697075416641>❌</emoji>"
-            lines.append(f"{state} `{key}` ({desc})")
+            lines.append(f"{state} <code>{key}</code> ({desc})")
         await utils.answer(message, f"<blockquote><emoji document_id=5258096772776991776>⚙</emoji> Конфигурация <b>{name}</b>:\n\n" + "\n".join(lines) + "</blockquote>")
 
     @loader.command(ru_doc="(название) — удалить конфигурацию", en_doc="(name) — delete a configuration")
